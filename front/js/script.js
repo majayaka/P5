306abda95605d65e79657d9339fd3441bf019c47
majayaka <yumotoayaka@gmail.com>
2022-09-26 17:47:40 +0200

    17h47 26th Sep

--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,26 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => addProducts(data))
+    .then((data) => {
+        console.log(data)
+        return addProducts(data)
+    })
 
     function addProducts(products) {
-        const imageUrl = products[0].imageUrl
-        
+        const id = products[0]._id
+        const anchor = makeAnchor(id)
+        appendChildren(anchor)
+    }
+
+    
+    function makeAnchor(id) {
         const anchor = document.createElement("a")
-        anchor.href = imageUrl
-        anchor.text = "Kanap Sinopé"
+        anchor.href = "./product.html?id=" + id
+        return anchor
+    }
+    
+    function appendChildren(anchor) {
         const items = document.querySelector("#items")
         items.appendChild(anchor)
     }
 
-
+    
