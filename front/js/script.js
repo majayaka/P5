6fdaee87e26f0448153a52e3598b088f678f6621
majayaka <yumotoayaka@gmail.com>
2022-09-27 13:25:54 +0200

    installed all products

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,35 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        return addProducts(data)
-    })
+    .then((data) => addProducts(data))
 
     function addProducts(products) {
-        const id = products[0]._id
-        const anchor = makeAnchor(id)
-        appendChildren(anchor)
+        // const _id = products[0]._id
+        // const imageUrl = products[0].imageUrl
+        // const altTxt = products[0].altTxt
+        // const name = products[0].name
+        // const description = products[0].description
+       products.forEach((kanap) => {
+           const {_id, imageUrl, altTxt, name, description} = kanap
+           const anchor = makeAnchor(_id)
+   
+           const article = document.createElement("article")
+           const image = makeImage(imageUrl, altTxt)
+           const h3 = makeH3(name)
+           const p =  makeParagraph(description)
+   
+          
+           appendArticleToAnchor(anchor, article)
+           appendElementsToArticle(article, [image, h3, p]) 
+        })
+   
+       }
+        
+    
+    function appendElementsToArticle(article, array) {
+        array.forEach((item)=> {
+            article.appendChild(item)
+        });
     }
-
     
     function makeAnchor(id) {
         const anchor = document.createElement("a")
@@ -18,9 +37,30 @@ fetch("http://localhost:3000/api/products")
         return anchor
     }
     
-    function appendChildren(anchor) {
+    function appendArticleToAnchor(anchor, article) {
         const items = document.querySelector("#items")
         items.appendChild(anchor)
+        anchor.appendChild(article)
     }
 
+    function makeImage(imageUrl, altTxt) {
+        const image = document.createElement("img")
+        image.src = imageUrl
+        image.alt = altTxt
+        return image
+    }
+
+    function makeH3(name) {
+       const h3 = document.createElement("h3")
+       h3.textContent = name
+       h3.classList.add("productName")
+       return h3
+    }
+
+    function makeParagraph(description) {
+        const p = document.createElement("p")
+        p.textContent = description
+        p.classList.add("productDescrirption")
+        return p
+    }
     
